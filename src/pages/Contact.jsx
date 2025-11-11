f0c2330ca2594e1e0ec07f3259d6e39892761d05
wasteddorg <wastedd.org@gmail.com>
2025-11-12 04:08:49 +0600

    Add Contact page with email information

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div className="page">
+      <h1>Contact</h1>
+      <p>For inquiries, please reach us at: <a href="mailto:contact@shikor.org">contact@shikor.org</a></p>
+      <p>(This is a placeholder address.)</p>
+    </div>
+  )
+}
